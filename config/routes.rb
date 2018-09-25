05ba0b9ae800c4f1c8c0d2bfda04548e751dabea
Aaron Lifton <aaronlifton@gmail.com>
2018-09-25 13:42:58 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 SolidusBraintree::Engine.routes.draw do
-  namespace :api, defaults: { format: 'json' } do
-    resource  :payment_client_token, only: [:create], controller: 'braintree_client_token'
-  end
+  resource  :payment_client_token, only: [:create], controller: 'braintree_client_token'
 end
