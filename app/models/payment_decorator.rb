f0fa1535bca53b237cb531d8302df64fcbc06648
Aaron Lifton <aaronlifton@gmail.com>
2019-01-06 18:46:53 -0800

    update payment decorator to send merchant_account_id

--- app/models/payment_decorator.rb
+++ app/models/payment_decorator.rb
@@ -1,2 +1,12 @@
 Spree::Payment.include SolidusBraintree::PaymentBraintreeNonceConcern
 Spree::Payment.include SolidusBraintree::InjectDeviceDataConcern
+
+Spree::Payment.class_eval do
+  private
+  # ensure merchant_account_id is passed to braintree
+  def gateway_options
+    super.merge({
+      merchant_account_id: ENV.fetch('BRAINTREE_MERCHANT_ACCOUNT_ID')
+    })
+  end
+end
